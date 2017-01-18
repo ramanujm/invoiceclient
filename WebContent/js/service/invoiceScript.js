bccf5f11dc3d3d529ed01d2b9d7585d5af14df45
ramanujm <ramanujm@yahoo.com>
2017-01-17 20:38:24 -0800

    Update invoiceScript.js

--- WebContent/js/service/invoiceScript.js
+++ WebContent/js/service/invoiceScript.js
@@ -1,13 +1,52 @@
- // here initialize the app
-  var myApp = new Framework7({
- animateNavBackIcon:true
-  });
+// here initialize the app
+var _APP_TITLE = 'Invoice';
+var myApp = new Framework7({
+	animateNavBackIcon : true
+});
 
- // If your using custom DOM library, then save it to $$ variable
- var $$ = Dom7;
+// If your using custom DOM library, then save it to $$ variable
+var $$ = Dom7;
 
- // Add the view
- var mainView = myApp.addView('.view-main', {
- // enable the dynamic navbar for this view
-     dynamicNavbar: true
-     });
+// Add the view
+var mainView = myApp.addView('.view-main', {
+	// enable the dynamic navbar for this view
+	dynamicNavbar : true
+});
+var mainView = myApp.addView('.view-main');
+// mainView.router.loadPage("viewPage/estimate.html");
+$('.form-from-data').on('click',function(e) {
+			e.preventDefault();
+
+			myApp.showPreloader();
+
+			var form_url = $(this).attr('action');
+			var form_data = myApp.formToData('#form-register');
+
+			var form_ajax = $.ajax({
+				url : form_url,
+				type : 'POST',
+				dataType : 'json',
+				data : form_data
+			});
+
+			form_ajax.done(function(response) {
+				if (response.result === 'success') {
+					mainView.router.loadPage({
+						url : 'viewPage/estimate.html',
+						ignoreCache : true,
+						reload : true
+					});
+					mainView.router.refreshPage();
+				} else {
+					myApp.alert('Your access data is incorrect, please try again.',	_APP_TITLE);
+				}
+			});
+
+			form_ajax.fail(function() {
+				myApp.alert('Error trying to register to the system.');
+			});
+
+			form_ajax.always(function() {
+				myApp.hidePreloader();
+			});
+		});
